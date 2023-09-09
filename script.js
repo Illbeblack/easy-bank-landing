1d75d329bcef389950f98d1a1e948ace1dfdbbd8
Illbeblack <shvartsdenis@mail.ru>
2023-09-09 21:52:06 +0300

    feat: add Smooth page navigation, Tabs changes,  Navigation animation

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+//   ELEMENTS
 
 const modalWindow = document.querySelector('.modal-window');
 const overlay = document.querySelector('.overlay');
@@ -9,6 +8,20 @@ const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
 const btnsOpenModalWindow = document.querySelectorAll(
   '.btn--show-modal-window'
 );
+
+//
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+//
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabContent = document.querySelectorAll('.operations__content');
+
+//
+const nav = document.querySelector('.nav');
+
+// Modal window
 
 const openModalWindow = function (e) {
   e.preventDefault();
@@ -33,11 +46,94 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-//   Add soft scroll
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+//   Add smooth scroll
 
 btnScrollTo.addEventListener('click', function(e) {
   section1.scrollIntoView({behavior: 'smooth'});
 })
+
+//   Smooth page navigation
+/*   Old solution
+
+document.querySelectorAll('.nav__link').forEach(function(htmlElement) {
+  htmlElement.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    document.querySelector(href).scrollIntoView({behavior: 'smooth'});
+  })
+});
+
+*/
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const href = e.target.getAttribute('href');
+    document.querySelector(href).scrollIntoView({behavior: 'smooth'});
+  }
+})
+
+//   Tabs
+
+tabContainer.addEventListener('click', function (e) {
+  const clickedButton = e.target.closest('.operations__tab');
+
+  //   Guard clause
+  if(!clickedButton) return;
+
+  //   Active tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'))
+  clickedButton.classList.add('operations__tab--active');
+
+  //   Active content
+  tabContent.forEach(content => content.classList.remove('operations__content--active'));
+  document.querySelector(`.operations__content--${clickedButton.dataset.tab}`).classList.add('operations__content--active');
+})
+
+//   Navigation animation
+
+const navLinksHoverAnimation = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const linkOver = e.target;
+    const siblingLinks = linkOver.closest('.nav__links').querySelectorAll('.nav__link');
+    const logo = linkOver.closest('.nav').querySelector('img');
+    const logoText = linkOver.closest('.nav').querySelector('.nav__text')
+
+    siblingLinks.forEach(el => {
+      if(el !== linkOver) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+    logoText.style.opacity = opacity;
+  }
+}
+
+nav.addEventListener('mouseover', function (e) {
+  navLinksHoverAnimation(e, 0.4);
+});
+
+nav.addEventListener('mouseout', function (e) {
+  navLinksHoverAnimation(e, 1);
+});
+
+/*   New solution with BIND() method and THIS
+
+const navLinksHoverAnimation = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const linkOver = e.target;
+    const siblingLinks = linkOver.closest('.nav__links').querySelectorAll('.nav__link');
+    const logo = linkOver.closest('.nav').querySelector('img');
+    const logoText = linkOver.closest('.nav').querySelector('.nav__text')
+
+    siblingLinks.forEach(el => {
+      if(el !== linkOver) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+    logoText.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', navLinksHoverAnimation.bind(0.4));
+nav.addEventListener('mouseout', navLinksHoverAnimation.bind(1));
+*/
+
+
